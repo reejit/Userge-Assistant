640a786cfe19fb86e8d89f8b652b3641f4033830
reejit <70316178+reejit@users.noreply.github.com>
2020-11-25 09:04:13 +0530

    Update config.py

--- assistant/config.py
+++ assistant/config.py
@@ -21,7 +21,7 @@ class Config:
     APP_ID = int(os.environ.get("APP_ID", 0))
     API_HASH = os.environ.get("API_HASH")
     BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    AUTH_CHATS = set([l-1001382153442])  # @UserGeOt
+    AUTH_CHATS = set([-1001382153442])  # @UserGeOt
     if os.environ.get("AUTH_CHATS"):
         AUTH_CHATS.update(map(int, os.environ.get("AUTH_CHATS").split()))
     WHITELIST_CHATS = set([-1001382153442])  # @UserGeSpam
